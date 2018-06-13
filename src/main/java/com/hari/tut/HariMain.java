9ce853885d29dc4077833a62bfc2b5575db33ee1
arunangsu.sahu <arnab.pradhan@abzooba.com>
2018-06-13 14:28:24 +0530

    First Commit

---
+++ src/main/java/com/hari/tut/HariMain.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.hari.tut;
+
+/**
+ * @author arunangsu.sahu
+ *
+ */
+public class HariMain {
+
+  /**
+   * 
+   */
+  public HariMain() {
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hari");
+
+  }
+
+}
